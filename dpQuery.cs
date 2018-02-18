f24a552ae4834a2b266338f962b0564ec471e9da
Home <dpv925@gmail.com>
2018-02-18 11:54:59 +0300

    Async methods added

--- dpQuery.cs
+++ dpQuery.cs
@@ -4,118 +4,51 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 
-public class DpQuery
+namespace DpQuery
 {
-
-	private string connectionString;
-
-	#region FillDataSet
-	public async Task<DataSet> FillDataSet(string sql, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
+	public class Qs
 	{
 
-		using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
+		private readonly ConnectionStringSettings connectionStringSettings;
+
+		public Qs()
 		{
-			await conn.OpenAsync().ConfigureAwait(false);
+	
+		}
 
-			using (SqlCommand dbCommand = new SqlCommand(sql, conn))
+		public Qs(ConnectionStringSettings connectionStringSettings)
+		{
+			this.connectionStringSettings = connectionStringSettings;
+		}
+
+		#region FillDataSetAsync
+		public async Task<DataSet> FillDataSetAsync(string sql, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
+		{
+
+			using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
 			{
-				dbCommand.CommandType = commandType;
-				addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
+				await conn.OpenAsync().ConfigureAwait(false);
 
-				using (SqlDataAdapter da = new SqlDataAdapter(dbCommand))
+				using (SqlCommand dbCommand = new SqlCommand(sql, conn))
 				{
-					var result = new DataSet();
-					da.Fill(result);
-					return result;
+					dbCommand.CommandType = commandType;
+					addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
+
+					using (SqlDataAdapter da = new SqlDataAdapter(dbCommand))
+					{
+						var result = new DataSet();
+						da.Fill(result);
+						return result;
+					}
 				}
 			}
 		}
-	}
-	#endregion
+		#endregion
 
-	#region FillDataTable
-	public async Task<DataTable> FillDataTable(string sql, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
-	{
-		using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
+		#region ExecuteNonQueryInTransaction
+		public async Task ExecuteNonQueryInTransactionAsync(string sql, SqlTransaction transaction, Action<SqlParameterCollection> addAdditionalParametersAction, CommandType commandType = CommandType.StoredProcedure)
 		{
-			await conn.OpenAsync().ConfigureAwait(false);
-
-			using (SqlCommand dbCommand = new SqlCommand(sql, conn))
-			{
-				dbCommand.CommandType = commandType;
-				addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
-
-				using (SqlDataAdapter da = new SqlDataAdapter(dbCommand))
-				{
-					var result = new DataTable();
-					da.Fill(result);
-					return result;
-				}
-			}
-		}
-	}
-
-	public async Task<DataTable> FillDataTable(Func<string> sqlGet, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
-	{
-		using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
-		{
-			await conn.OpenAsync().ConfigureAwait(false);
-
-			using (SqlCommand dbCommand = new SqlCommand(sqlGet(), conn))
-			{
-				dbCommand.CommandType = commandType;
-				addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
-
-				using (SqlDataAdapter da = new SqlDataAdapter(dbCommand))
-				{
-					var result = new DataTable();
-					da.Fill(result);
-					return result;
-				}
-			}
-
-		}
-	}
-	#endregion
-
-	#region _GetConnectionString
-	private string _GetConnectionString()
-	{
-		if (string.IsNullOrEmpty(connectionString))
-		{
-			connectionString = ConfigurationManager.ConnectionStrings["Default"]?.ConnectionString;
-		}
-		return connectionString;
-	}
-	#endregion
-
-	#region ExecuteScalar
-	public async Task<object> ExecuteScalar(string sql, Action<SqlParameterCollection> addAdditionalParametersAction = null,
-		CommandType commandType = CommandType.StoredProcedure)
-	{
-		using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
-		{
-			await conn.OpenAsync().ConfigureAwait(false);
-
-			using (SqlCommand dbCommand = new SqlCommand(sql, conn))
-			{
-				dbCommand.CommandType = commandType;
-				addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
-
-				return await dbCommand.ExecuteScalarAsync().ConfigureAwait(false);
-			}
-		}
-	}
-	#endregion
-
-	#region ExecuteNonQuery
-	public async Task ExecuteNonQuery(string sql, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
-	{
-		using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
-		{
-			await conn.OpenAsync().ConfigureAwait(false);
-
-			using (SqlCommand dbCommand = new SqlCommand(sql, conn))
+			using (SqlCommand dbCommand = new SqlCommand(sql, transaction.Connection))
 			{
 				dbCommand.CommandType = commandType;
 				addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
@@ -123,29 +56,210 @@ public class DpQuery
 				await dbCommand.ExecuteNonQueryAsync().ConfigureAwait(false);
 			}
 		}
-	}
-	#endregion
+		#endregion
 
-	#region ExecuteReader
-	public async Task ExecuteReader(string sql, Action<SqlParameterCollection> addAdditionalParametersAction, Action<SqlDataReader> ReaderAction, CommandType commandType = CommandType.StoredProcedure)
-	{
-		using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
+		#region BeginTransaction
+		public async Task BeginTransaction(Action<SqlTransaction> execute)
 		{
-			await conn.OpenAsync().ConfigureAwait(false);
+			using (SqlConnection connection = new SqlConnection(_GetConnectionString()))
+			{
+				await connection.OpenAsync().ConfigureAwait(false);
 
-			using (SqlCommand dbCommand = new SqlCommand(sql, conn))
+				using (SqlTransaction transaction = connection.BeginTransaction())
+				{
+					try
+					{
+						execute.Invoke(transaction);
+						transaction.Commit();
+					}
+					catch (Exception ex)
+					{
+						transaction.Rollback();
+						throw;
+					}
+
+				}
+
+			} // using SQLiteConnection
+		}
+		#endregion
+
+		#region ExecuteInTransactionAsync
+		public async Task ExecuteInTransactionAsync(Action transactionBody, CommandType commandType = CommandType.StoredProcedure)
+		{
+			using (SqlConnection connection = new SqlConnection(_GetConnectionString()))
+			{
+				await connection.OpenAsync().ConfigureAwait(false);
+
+				using (SqlTransaction transaction = connection.BeginTransaction())
+				{
+					try
+					{
+						transactionBody.Invoke();
+						transaction.Commit();
+					}
+					catch
+					{
+						transaction.Rollback();
+						throw;
+					}
+					transaction.Commit();
+				}
+			}
+		}
+		#endregion
+
+
+
+		#region FillDataTableAsync
+		public DataTable FillDataTableInTransaction(string sql, SqlTransaction transaction, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
+		{
+			using (SqlCommand dbCommand = new SqlCommand(sql, transaction.Connection))
 			{
 				dbCommand.CommandType = commandType;
 				addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
 
-				SqlDataReader Reader = await dbCommand.ExecuteReaderAsync(CommandBehavior.CloseConnection).ConfigureAwait(false);
-				ReaderAction?.Invoke(Reader);
-				Reader.Close();
+				using (SqlDataAdapter da = new SqlDataAdapter(dbCommand))
+				{
+					var result = new DataTable();
+					da.Fill(result);
+					return result;
+				}
+			}
+		}
+
+		public async Task<DataTable> FillDataTableAsync(string sql, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
+		{
+			using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
+			{
+				await conn.OpenAsync().ConfigureAwait(false);
+
+				using (SqlCommand dbCommand = new SqlCommand(sql, conn))
+				{
+					dbCommand.CommandType = commandType;
+					addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
+
+					using (SqlDataAdapter da = new SqlDataAdapter(dbCommand))
+					{
+						var result = new DataTable();
+						da.Fill(result);
+						return result;
+					}
+				}
+			}
+		}
+		public DataTable FillDataTableInTransaction(Func<string> sqlGet, SqlTransaction transaction, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
+		{
+			using (SqlCommand dbCommand = new SqlCommand(sqlGet(), transaction.Connection))
+			{
+				dbCommand.CommandType = commandType;
+				addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
+
+				using (SqlDataAdapter da = new SqlDataAdapter(dbCommand))
+				{
+					var result = new DataTable();
+					da.Fill(result);
+					return result;
+				}
+			}
+		}
+
+		public async Task<DataTable> FillDataTableAsync(Func<string> sqlGet, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
+		{
+			using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
+			{
+				await conn.OpenAsync().ConfigureAwait(false);
+
+				using (SqlCommand dbCommand = new SqlCommand(sqlGet(), conn))
+				{
+					dbCommand.CommandType = commandType;
+					addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
+
+					using (SqlDataAdapter da = new SqlDataAdapter(dbCommand))
+					{
+						var result = new DataTable();
+						da.Fill(result);
+						return result;
+					}
+				}
 
 			}
 		}
-	}
-	#endregion
+		#endregion
 
+		#region _GetConnectionString
+		private string _GetConnectionString()
+		{
+
+			if (connectionStringSettings != null)
+				return connectionStringSettings.ConnectionString;
+
+			ConnectionStringSettings xc = ConfigurationManager.ConnectionStrings["Default"];
+
+			return ConfigurationManager.ConnectionStrings["Default"]?.ConnectionString;
+			
+		}
+		#endregion
+
+		#region ExecuteScalarAsync
+		public async Task<object> ExecuteScalarAsync(string sql, Action<SqlParameterCollection> addAdditionalParametersAction = null,
+			CommandType commandType = CommandType.StoredProcedure)
+		{
+			using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
+			{
+				await conn.OpenAsync().ConfigureAwait(false);
+
+				using (SqlCommand dbCommand = new SqlCommand(sql, conn))
+				{
+					dbCommand.CommandType = commandType;
+					addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
+
+					return await dbCommand.ExecuteScalarAsync().ConfigureAwait(false);
+				}
+			}
+		}
+		#endregion
+
+		#region ExecuteNonQueryAsync
+		public async Task ExecuteNonQueryAsync(string sql, Action<SqlParameterCollection> addAdditionalParametersAction = null, CommandType commandType = CommandType.StoredProcedure)
+		{
+			using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
+			{
+				await conn.OpenAsync().ConfigureAwait(false);
+
+				using (SqlCommand dbCommand = new SqlCommand(sql, conn))
+				{
+					dbCommand.CommandType = commandType;
+					addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
+
+					await dbCommand.ExecuteNonQueryAsync().ConfigureAwait(false);
+				}
+			}
+		}
+		#endregion
+
+		#region ExecuteReaderAsync
+		public async Task ExecuteReaderAsync(string sql, Action<SqlParameterCollection> addAdditionalParametersAction, Action<SqlDataReader> ReaderAction, CommandType commandType = CommandType.StoredProcedure)
+		{
+			using (SqlConnection conn = new SqlConnection(_GetConnectionString()))
+			{
+				await conn.OpenAsync().ConfigureAwait(false);
+
+				using (SqlCommand dbCommand = new SqlCommand(sql, conn))
+				{
+					dbCommand.CommandType = commandType;
+					addAdditionalParametersAction?.Invoke(dbCommand.Parameters);
+
+					SqlDataReader Reader = await dbCommand.ExecuteReaderAsync(CommandBehavior.CloseConnection).ConfigureAwait(false);
+					ReaderAction?.Invoke(Reader);
+					Reader.Close();
+
+				}
+			}
+		}
+		#endregion
+
+
+	}
 
 }
