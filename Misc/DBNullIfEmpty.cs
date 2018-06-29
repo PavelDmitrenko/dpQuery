ed790da288fe2bb75fd5adccc50cae92a209a601
dmitrenko <dpv925@gmail.com>
2018-06-29 09:17:42 +0300

    NullIfEmpty added

---
+++ Misc/DBNullIfEmpty.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace dpQuery
+{
+	public static class DBNullIfEmpty
+	{
+		public static object Int(int? value)
+		{
+			if (!value.HasValue)
+				return DBNull.Value;
+
+			if (value.Value == 0)
+				return DBNull.Value;
+
+			return value.Value;
+		}
+
+		public static object String(string value)
+		{
+			if (string.IsNullOrEmpty(value))
+				return DBNull.Value;
+
+			return value;
+		}
+
+		public static object Date(DateTime? value)
+		{
+			if (!value.HasValue)
+				return DBNull.Value;
+
+			if (value.Value.Equals(DateTime.MaxValue) || value.Value.Equals(DateTime.MinValue))
+				return DBNull.Value;
+
+			if (value.Value.ToUniversalTime().Equals(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)))
+				return DBNull.Value;
+
+			return value.Value;
+		}
+	}
+}
